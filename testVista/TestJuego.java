d3cdd021215f0a195169d67f3a647a5f867dd652
Juan Camilo Narváez <ulvenforst@gmail.com>
2022-10-17 17:28:38 -0500

    Primera versión Ahorcado Univalle

---
+++ testVista/TestJuego.java
@@ -0,0 +1,13 @@
+// import logica.Juego;
+
+// public class TestJuego {
+//     public static void main(String[] args) {
+//         Juego juego1 = new Juego("Test1");
+//         Juego juego2 = new Juego("Test2");
+//         Juego juego3 = new Juego("Test3");
+//         Juego juego4 = new Juego("Test4");
+         
+
+        
+//     }   
+// }
